f2963515fce69579b291f83eceb5cf9541388b1f
Mr WhiteKidd <austinslater58@gmail.com>
2022-05-26 05:04:49 -0500

    able to push a new player to the _players object

--- app.js
+++ app.js
@@ -1,38 +1,55 @@
 const team = {
-    _players : [
-     {
-       firstName: 'abc',
-       lastName: 'zxy',
-       age: 3,
+    _players: [
+        {
+            firstName: 'abc',
+            lastName: 'zxy',
+            age: 3,
+        },
+        {
+            firstName: 'ccb',
+            lastName: 'zzy',
+            age: 33,
+        },
+        {
+            firstName: 'bnb',
+            lastName: 'zynth',
+            age: 333,
+        },
+    ],
+    _games: [
+        {
+            opponent: "blue",
+            teamPoints: "25",
+            opponentPoints: "26",
+        },
+        {
+            opponent: "blue",
+            teamPoints: "222",
+            opponentPoints: "221",
+        },
+        {
+            opponent: "blue",
+            teamPoints: "2214",
+            opponentPoints: "22441",
+        },
+    ],
+    get players() {
+        return this._players;
     },
-    {
-        firstName: 'ccb',
-       lastName: 'zzy',
-       age: 33,
+    get games() {
+        return this._games;
     },
-    {
-        firstName: 'bnb',
-       lastName: 'zynth',
-       age: 333,
+    addPlayer(newFirstName, newLastName, newAge) {
+        let player = {
+            firstName: newFirstName,
+            lastName: newLastName,
+            age: newAge,
+        };
+        this.players.push(player);
     },
-   ],
-   _games  : [
-    {
-        opponent: "blue",
-        teamPoints: "25",
-        opponentPoints: "26",
-      },
-      {
-        opponent: "blue",
-        teamPoints: "222",
-        opponentPoints: "221",
-      },
-      {
-        opponent: "blue",
-        teamPoints: "2214",
-        opponentPoints: "22441",
-      },
-   ],
-  }
-  
-  console.log(team._players);
+}
+
+
+team.addPlayer('duke', 'dukeski', 24);
+console.log(team._players);
+console.log(team._games);
