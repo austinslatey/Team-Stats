75fc6c59bfb6430ab99156ddafad3440b2473e88
Mr WhiteKidd <austinslater58@gmail.com>
2022-05-26 04:45:25 -0500

    made a team object with a players and games array only players has data

---
+++ app.js
@@ -0,0 +1,24 @@
+const team = {
+    _players : [
+     {
+       firstName: 'abc',
+       lastName: 'zxy',
+       age: 3,
+    },
+    {
+        firstName: 'ccb',
+       lastName: 'zzy',
+       age: 33,
+    },
+    {
+        firstName: 'bnb',
+       lastName: 'zynth',
+       age: 333,
+    },
+   ],
+   _games  : [
+  
+   ],
+  }
+  
+  console.log(team._players);
